a6e65264185ac39c0af35c65ca788140f459dad7
Create_first_repo <gauravchoundiye123@gmail.com>
2024-09-14 14:45:34 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useEffect ,useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const def = {tempreture : 0, condition : "Cool"}
+  const [data, setData] = useState(def);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setData({tempreture:25,condition:"sunney"})
+    },1000)
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+        {data.tempreture>20?<div style={{color:"red"}}>
+          <p>tempreture:{data.tempreture}</p>
+          <p>condition:{data.condition}</p>
+        </div>:<div style={{color:"blue"}}>
+        <p>tempreture:{data.tempreture}</p>  
+        <p>condition:{data.condition}</p>
+        </div>}   
     </div>
   )
 }
